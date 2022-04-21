c89ef14c64894c66f8cab3522ccf885902875c1c
LuizFilipeM <103970058+LuizFilipeM@users.noreply.github.com>
2022-04-21 15:31:14 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include "tp1.h"
 
+
 void main(){
     int num;
-    lista *list;
-    list = (lista*)malloc(sizeof(lista));
-
+    lista *list; 
+    printf("aqui");
     cria_lista(list);
-        printf("eh isso\n");
-
+    
 
     scanf("%d", &num);
+    list = int_to_big(num,list);
 
-    list = int_to_big(num);
+    printf("entao eh isso");
     
     imprimir(list);
-    printf("entao eh isso");
+    
 
 }
