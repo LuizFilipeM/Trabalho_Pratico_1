a24971f38211d0a3645cc419981d91cfb0a522f3
LuizFilipeM <103970058+LuizFilipeM@users.noreply.github.com>
2022-04-22 12:20:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,11 @@
 #include <stdlib.h>
 #include "tp1.h"
 
-
 void main(){
     int num;
     lista *list; 
-    printf("aqui");
     cria_lista(list);
-    
-
     scanf("%d", &num);
     list = int_to_big(num,list);
-
-    printf("entao eh isso");
-    
     imprimir(list);
-    
-
 }
